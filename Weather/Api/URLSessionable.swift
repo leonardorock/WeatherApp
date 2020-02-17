8aaa0a7e65f2591bb2f0c5fbf4f398d1b4753775
Leonardo Oliveira <leonardorock@icloud.com>
2020-02-17 10:27:34 -0300

    Adding documentation markup

--- Weather/Api/URLSessionable.swift
+++ Weather/Api/URLSessionable.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 public protocol URLSessionable {
+    /// Performs a download task for the given request asynchronously
+    /// - Parameters:
+    ///   - request: The URLRequest
+    ///   - completionHandler: A completion block
+    /// - Note: Some custom note
     func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
 }
 
